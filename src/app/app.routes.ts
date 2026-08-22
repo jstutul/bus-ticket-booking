54bef26b80ded12c7ae3e50ada9ea261ecaf4bee
Jafor Sadik Tutul <tutul@ucasbd.com>
2026-08-23 01:51:35 +0600

    search

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
 import { Common } from './core/layouts/common/common';
 import { Home } from './core/pages/home/home';
+import { Search } from './core/pages/search/search';
 
 export const routes: Routes = [
     {
         path: '',
         component: Common,
         children: [
-            {path:'',component:Home}
+            { path: '', component: Home },
+            {path:'search',component:Search}
         ]
     }
 ];
